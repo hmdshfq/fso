eb061cb9426651b44035b618c21b25de59e5b3ec
Hammad <hmdshfq@outlook.com>
2024-11-10 00:11:31 +0100

    feat:add a simple web server using http api

--- part3/index.js
+++ part3/index.js
@@ -1,1 +1,30 @@
-console.log('hello world');
+import http from "http";
+
+const PORT = 3000;
+
+const notes = [
+    {
+        id: "1",
+        content: "HTML is easy",
+        important: true,
+    },
+    {
+        id: "2",
+        content: "Browser can execute only JavaScript",
+        important: false,
+    },
+    {
+        id: "3",
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true,
+    },
+];
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(notes));
+});
+
+server.listen(PORT);
+
+console.log(`Server is running on port ${PORT}`);
