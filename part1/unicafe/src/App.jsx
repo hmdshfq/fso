9d47b9d00c0f0488d76d1a9bca19f7baaff8cb55
Hammad <hmdshfq@outlook.com>
2024-11-11 16:50:34 +0100

    feat: exercise 1.6 — unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
-const App = () => {};
+const Button = ({ text, onClick }) => {
+    return <button onClick={onClick}>{text}</button>;
+};
+
+const Display = ({good, neutral, bad}) => {
+    return (
+        <>
+            <h2>Statistics</h2>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+        </>
+    );
+};
+
+const App = () => {
+    const [good, setGood] = React.useState(0);
+    const [neutral, setNeutral] = React.useState(0);
+    const [bad, setBad] = React.useState(0);
+
+    const increaseGood = () => setGood(good + 1);
+    const increaseNeutral = () => setNeutral(neutral + 1);
+    const increaseBad = () => setBad(bad + 1);
+
+    return (
+        <div>
+            <h1>Give feedback</h1>
+            <Button onClick={increaseGood} text="Good" />
+            <Button onClick={increaseNeutral} text="Neutral" />
+            <Button onClick={increaseBad} text="Bad" />
+            <Display good={good} neutral={neutral} bad={bad} />
+        </div>
+    );
+};
 
 export default App;
