279e3c6445d4ed16fdc6c1ec3483402562fc8603
Hammad <hmdshfq@outlook.com>
2024-11-11 17:13:27 +0100

    feat: exercise 1.7 — unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,28 +1,65 @@
-import React from "react";
+import { useState } from "react";
 
 const Button = ({ text, onClick }) => {
     return <button onClick={onClick}>{text}</button>;
 };
 
-const Display = ({good, neutral, bad}) => {
+const Display = ({
+    good,
+    neutral,
+    bad,
+    total,
+    average,
+    positivePercentage,
+}) => {
     return (
         <>
             <h2>Statistics</h2>
             <p>Good: {good}</p>
             <p>Neutral: {neutral}</p>
             <p>Bad: {bad}</p>
+            <p>All: {total}</p>
+            <p>Average: {average}</p>
+            <p>Positive Percentage: {positivePercentage}%</p>
         </>
     );
 };
 
 const App = () => {
-    const [good, setGood] = React.useState(0);
-    const [neutral, setNeutral] = React.useState(0);
-    const [bad, setBad] = React.useState(0);
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    const [total, setTotal] = useState(0);
+    const [average, setAverage] = useState(0);
+    const [positivePercentage, setPositivePercentage] = useState(0);
 
-    const increaseGood = () => setGood(good + 1);
-    const increaseNeutral = () => setNeutral(neutral + 1);
-    const increaseBad = () => setBad(bad + 1);
+    const increaseGood = () => {
+        const nextValue = good + 1;
+        setGood(nextValue);
+        setTotal(total + 1);
+        findAverage();
+        findPositivePercentage();
+    };
+    const increaseNeutral = () => {
+        const nextValue = neutral + 1;
+        setNeutral(nextValue);
+        setTotal(total + 1);
+        findAverage();
+        findPositivePercentage();
+    };
+    const increaseBad = () => {
+        const nextValue = bad + 1;
+        setBad(nextValue);
+        setTotal(total + 1);
+        findAverage();
+        findPositivePercentage();
+    };
+    const findAverage = () => {
+        setAverage((good - bad) / (total === 0 ? 1 : total));
+    };
+    const findPositivePercentage = () => {
+        setPositivePercentage((good / total) * 100);
+    };
 
     return (
         <div>
@@ -30,7 +67,14 @@ const App = () => {
             <Button onClick={increaseGood} text="Good" />
             <Button onClick={increaseNeutral} text="Neutral" />
             <Button onClick={increaseBad} text="Bad" />
-            <Display good={good} neutral={neutral} bad={bad} />
+            <Display
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={total}
+                average={average}
+                positivePercentage={positivePercentage}
+            />
         </div>
     );
 };
