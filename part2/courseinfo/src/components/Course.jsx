81cb6a342dae08b8bb3009d118358697275089fe
Hammad <hmdshfq@outlook.com>
2024-11-14 22:23:35 +0100

    feat: exercise 2.4 and 2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,13 +2,17 @@ import Header from './Header';
 import Content from './Content';
 import Total from './Total';
 
-const Course = ({course}) => {
+const Course = ({courses}) => {
     return (
-        <div>
-            <Header course={course.name}/>
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
+        courses.map( course => {
+            return (
+                <section key={course.id}>
+                    <Header course={course.name}/>
+                    <Content parts={course.parts} />
+                    <Total parts={course.parts} />
+                </section>
+            )
+        })
     )
 };
 
--- part2/courseinfo/src/components/Header.jsx
+++ part2/courseinfo/src/components/Header.jsx
@@ -1,5 +1,5 @@
 function Header({course}) {
-    return <h1>{course}</h1>;
+    return <h2>{course}</h2>;
 }
 
 export default Header;
