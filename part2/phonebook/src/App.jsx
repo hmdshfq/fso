71ee390af0c8dd71d3ba9fb579b1e85f1da38555
Hammad <hmdshfq@outlook.com>
2024-11-15 14:12:22 +0100

    feat: exercise 2.6 — Phonebook

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const App = () => {
+    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [newName, setNewName] = useState("Enter name");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const nextPersons = [...persons, { name: newName }];
+        setPersons(nextPersons);
+        setNewName('');
+    };
+
+    const handleChange = e => {
+        const nextNewName = e.target.value;
+        setNewName(nextNewName);
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    Name:{" "}
+                    <input
+                        value={newName}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <button type="submit">
+                        Add
+                    </button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map((person) => {
+                return <p key={person.name}>{person.name}</p>;
+            })}
+        </div>
+    );
+};
+
+export default App;
