0ba4267d861b96060a8e9578e63be27128e4520e
Hammad Shafiq <hmdshfq@outlook.com>
2024-11-24 20:08:23 +0100

    feat: 2.18 — data for countries

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 function App() {
     const [search, setSearch] = useState("");
     const [countries, setCountries] = useState([]);
+    const [country, setCountry] = useState({});
 
     const handleSearch = (event) => {
         event.preventDefault();
@@ -10,6 +11,9 @@ function App() {
     };
 
     useEffect(() => {
+        if (search === "") {
+            return;
+        }
         const timeoutId = setTimeout(() => {
             fetch("https://studies.cs.helsinki.fi/restcountries/api/all/")
                 .then((response) => response.json())
@@ -21,11 +25,24 @@ function App() {
                     const filteredNames = allCountriesNames.filter((country) =>
                         country.toLowerCase().includes(search)
                     );
-                    console.log(filteredNames);
-                    console.log(search);
                     setCountries(filteredNames);
+                    filteredNames.length === 1 &&
+                        fetch(
+                            `https://studies.cs.helsinki.fi/restcountries/api/name/${filteredNames[0]}`
+                        )
+                            .then((response) => response.json())
+                            .then((data) => {
+                                const nextCountry = { ...data };
+                                setCountry(nextCountry);
+                            })
+                            .catch((error) => {
+                                console.log(error);
+                            });
+                })
+                .catch((error) => {
+                    console.log(error);
                 });
-        }, 1000);
+        }, 500);
         return () => {
             clearTimeout(timeoutId);
         };
@@ -41,10 +58,58 @@ function App() {
                     type="text"
                     value={search}
                     onChange={handleSearch}
+                    autoFocus
                 />
             </form>
             <h2>Country Data</h2>
-            <p>{countries}</p>
+            {countries.length === 0 && (
+                <p>Please type the name of a country in the search</p>
+            )}
+            {countries.length > 10 && (
+                <p>Too many matches please specify your search</p>
+            )}
+            {countries.length <= 10 && countries.length > 1 && (
+                <ul>
+                    {countries.map((country) => (
+                        <li key={country}>{country}</li>
+                    ))}
+                </ul>
+            )}
+            {countries.length === 1 && Object.keys(country).length !== 0 && (
+                <div>
+                    <h3>{country.name.common}</h3>
+                    <figure>
+                        <img
+                            src={country.flags.svg}
+                            alt={country.flags.alt}
+                            width={250}
+                        />
+                        <figcaption></figcaption>
+                    </figure>
+                    <p>Capital(s):</p>
+                    <ul>
+                        {country.capital.map((c) => (
+                            <li key={c}>{c}</li>
+                        ))}
+                    </ul>
+                    <p>Area: {`${country.area.toLocaleString()} km²`}</p>
+                    <p>Population: {country.population.toLocaleString()}</p>
+                    <p>
+                        Currency name:{" "}
+                        {Object.values(country.currencies)[0].name}
+                    </p>
+                    <p>
+                        Currency symbol:{" "}
+                        {Object.values(country.currencies)[0].symbol}
+                    </p>
+                    <p>Language(s):</p>
+                    <ul>
+                        {Object.values(country.languages).map((language) => (
+                            <li key={language}>{language}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </>
     );
 }
