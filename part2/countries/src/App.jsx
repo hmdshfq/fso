3c46c545dc1d105a172dfa37202e5e1f5fe8eca6
Hammad Shafiq <hmdshfq@outlook.com>
2024-11-24 01:44:18 +0100

    feat: create App structure

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    // https://studies.cs.helsinki.fi/restcountries/
+    const [search, setSearch] = useState("");
+    const [countries, setCountries] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleSearch = (event) => {
+        event.preventDefault();
+        setSearch(event.target.value);
+    };
+
+    useEffect(() => {
+        
+    }, []);
+
+    return (
+        <>
+            <h1>Data for countries</h1>
+            <form>
+                <label htmlFor="search">Find countries: </label>
+                <input
+                    id="search"
+                    type="text"
+                    value={search}
+                    onChange={handleSearch}
+                />
+            </form>
+        </>
+    );
 }
 
-export default App
+export default App;
